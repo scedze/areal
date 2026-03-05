70f6bb6cbafab84642f98db4a14c0036023e3c89
scedze <ada.fomina.00.00@gmail.com>
2026-03-05 22:35:58 +0300

    увольнение сотрудник(бд)

--- core.php
+++ core.php
@@ -26,6 +26,24 @@ if($_SERVER["REQUEST_METHOD"] === 'POST' && isset( $_POST['action'])) {
         ]);
     }
     if ($_POST['action'] === 'update'){
-        
+        $email_array = '{'. $_POST['email'] . '}';
+        $sql = "UPDATE sotrudnik SET
+                    familiya = ?, imya = ?, otchestvo = ?, data_rojdeniya = ?,
+                    seriya_pasport = ?, nomer_pasport = ?, nomer_telefona = ?,
+                    email = ?, adres = ?, id_otdela = ?, id_doljnosti = ?,
+                    zarplata = ?, data_priema = ?
+                WHERE id_sotrudnik = ?";
+        $pdo -> prepare( $sql) -> execute([
+            $_POST['familiya'], $_POST['imya'], $_POST['otchestvo'], $_POST['data_rojdeniya'],
+            $_POST['seriya'], $_POST['nomer'], $_POST['nomer_telefona'],
+            $email_array, $_POST['adres'], $_POST['id_otdela'],
+            $_POST['id_doljnosti'], $_POST['zarplata'], $_POST['data_priema'],
+            $_POST['id_sotrudnik']
+        ]);
+
     } 
+    if ($_POST['action'] === 'fire'){
+        $stmt = $pdo -> prepare("UPDATE sotrudnik SET status = false, data_uvolneniya = CURRENT_DATE WHERE id_sotrudnik = ?");
+        $stmt -> execute([$_POST['ID']]);
+    }
 } 
